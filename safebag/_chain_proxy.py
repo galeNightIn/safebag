cd0f79c41f4e21750560d1a4336c2cfd9298f34b
Alexandr Solovev <nightingale.alex.info@gmail.com>
2022-05-15 13:34:04 +0300

    Add docstrings

--- safebag/_chain_proxy.py
+++ safebag/_chain_proxy.py
@@ -2,13 +2,27 @@ from __future__ import annotations
 
 import typing
 
-_DATA_PROXY_SLOTS = ('__data_obj__', '__bool_hook__')
-
 T = typing.TypeVar('T')
 V = typing.TypeVar('V')
 
+_DATA_PROXY_SLOTS = ('__data_obj__', '__bool_hook__')
+
 
 class ChainProxy(typing.Generic[T]):
+    """
+    `ChainProxy` container:
+        * stores `data_object`
+        * proxying `data_object` attribute value into new `ChainProxy` instance
+        when attribute is invoked. If attribute does not exist or attribute value is `None`
+
+    `ChainProxy` instance always returning when attribute is invoked
+
+    :param data_object: Data proxied by `ChainProxy`
+    :type data_object: T
+    :param: bool_hook: Used in `ChainProxy` instance bool cast.
+    bool(chain_proxy_instance) == bool_hook
+    :type: bool
+    """
     __slots__ = _DATA_PROXY_SLOTS
 
     def __init__(self, data_object: T, *, bool_hook: bool = False) -> None:
@@ -16,7 +30,7 @@ class ChainProxy(typing.Generic[T]):
         self.__bool_hook__ = bool_hook
 
     def __getattr__(self, attr: str) -> ChainProxy[T]:
-        object_attribute = getattr(self.__data_obj__, attr, None)
+        object_attribute = getattr(self.__data_obj__, attr)
 
         if object_attribute is None:
             return ChainProxy(object_attribute, bool_hook=False)
@@ -24,7 +38,7 @@ class ChainProxy(typing.Generic[T]):
         return ChainProxy(object_attribute, bool_hook=True)
 
     def __get_value__(
-            self, *, default: typing.Optional[V] = None
+        self, *, default: typing.Optional[V] = None
     ) -> typing.Union[T, V]:
         if not self and default is not None:
             return default
@@ -41,6 +55,14 @@ class ChainProxy(typing.Generic[T]):
 
 
 def _get_value(
-        chain_proxy: ChainProxy, *, default: typing.Optional[V]
+    chain_proxy: ChainProxy[T], *, default: typing.Optional[V] = None
 ) -> typing.Union[T, V]:
+    """Protected method for withdrawal object from `ChainProxy`
+    :param chain_proxy: `ChainProxy` instance
+    :type chain_proxy: ChainProxy[T]
+    :param default: Value returned if object is None (default is None)
+    :type default: Optional[V]
+    :returns: data_object from `ChainProxy`
+    :rtype: T or V
+    """
     return chain_proxy.__get_value__(default=default)
--- safebag/_methods.py
+++ safebag/_methods.py
@@ -7,6 +7,32 @@ V = typing.TypeVar('V')
 
 
 def chain(data_object: T) -> ChainProxy[T]:
+    """Optional chain constructor
+
+    Optional chain constructed from any object.
+    Chain is used for building sequence of null-safe attribute calls.
+
+    :Example:
+        >>> from __future__ import annotations
+        >>> import dataclasses as dt
+        >>> import typing
+        >>> @dt.dataclass
+        ... class Node:
+        ...     data: int
+        ...     node: typing.Optional[Node]
+        >>> nodes = Node(data=1, node=Node(data=2, node=None))
+        >>>
+        >>> from safebag import chain
+        >>> from safebag import chain, get_value
+        >>> third_node_proxy = chain(nodes).node.node.node
+        >>> third_node_proxy
+        ChainProxy(data_object=None, bool_hook=False)
+
+    :param data_object: Any object for optional chaining
+    :type data_object: T
+    :return: `ChainProxy` for data_object
+    :rtype ChainProxy[T]
+    """
     return ChainProxy(data_object)
 
 
@@ -15,4 +41,38 @@ def get_value(
         *,
         default: typing.Optional[V] = None
 ) -> typing.Union[T, V, None]:
+    """Final value getter for optional chain.
+
+    Optional chain constructed from any object.
+    Chain is used for building sequence of null-safe attribute calls.
+
+    :Example:
+        >>> from __future__ import annotations
+        >>> import dataclasses as dt
+        >>> import typing
+        >>> @dt.dataclass
+        ... class Node:
+        ...     data: int
+        ...     node: typing.Optional[Node]
+        >>> nodes = Node(data=1, node=Node(data=2, node=None))
+        >>>
+        >>> from safebag import chain
+        >>> from safebag import chain, get_value
+        >>> third_node_proxy = chain(nodes).node.node.node
+
+        >>> value = get_value(third_node_proxy)
+        >>> assert value is None
+
+        >>> next_node = chain(nodes).node
+        >>> value = get_value(next_node)
+        >>> value
+        Node(data=2, node=None)
+
+    :Useful in combination with walrus operator:
+        >>> if next_node := chain(nodes).node.node:
+        ...     print(get_value(next_node))
+        >>> if next_node := chain(nodes).node:
+        ...     print(get_value(next_node))
+        Node(data=2, node=None)
+    """
     return _get_value(proxy_object, default=default)
